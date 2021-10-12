e0a671c547c92de9743641540542cb086995b587
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-12 20:14:35 +0300

    Created ship-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShipDetailComponent } from './ship-detail/ship-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShipDetailComponent
   ],
   imports: [
     BrowserModule,

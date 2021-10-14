a50fb5f472a231c092dd26afaec07e96d2f61558
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-14 22:19:49 +0300

    updated mqtt service

--- src/app/mqtt.service.ts
+++ src/app/mqtt.service.ts
@@ -1,11 +1,54 @@
 import { Injectable } from '@angular/core';
+import { Paho } from 'ng2-mqtt/mqttws31';
+import { ShipService } from './ship.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MqttService {
   private client: any;
+  public connectionStatus: boolean = false;
+  private connectionProperties: any = {
+    onSuccess: this.onConnect.bind(this),
+    mqttVersion: 4,
+    useSSL: true,
+    userName: "digitraffic",
+    password: "digitrafficPassword"
+  };
 
-  constructor() { }
+  constructor(private shipService: ShipService) { }
 
+
+  connect() {
+    this.client = new Paho.MQTT.Client("meri.digitraffic.fi", 61619, "ShipRadar");
+    this.client.onMessageArrived = this.onMessageArrived.bind(this);
+    this.client.onConnectionLost = this.onConnectionLost.bind(this);
+    this.client.connect(this.connectionProperties);
+  }
+
+  onConnect() {
+    this.connectionStatus = true;
+    console.log('Websocket Connected');
+  }
+
+  subscribe(topic: string) {
+    this.client.subscribe(topic);
+  }
+
+  unsubscribe(topic:string) {
+    this.client.unsubscribe(topic);
+  }
+
+  onConnectionLost(responseObject: any) {
+    this.connectionStatus = false;
+    if (responseObject.errorCode !== 0) {
+      console.log('onConnectionLost:' + responseObject.errorMessage);
+    }
+  }
+
+  onMessageArrived(message: any) {
+    console.log("MessageArrived ", message);
+    let ship = JSON.parse(message.payloadString);
+    return ship;
+  }
 }

497bd51d4ea21eeed5c7813b0b6453883ede4f50
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-16 23:23:27 +0300

    Centers the map when clicked

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -11,18 +11,26 @@ import { interval } from 'rxjs';
 export class MapsComponent implements OnInit {
   @Input() ships: Ship[] = [];
   center: google.maps.LatLngLiteral = { lat: 61.061435, lng: 28.320379 };
-  zoom = 12;
+  zoom = 11;
   markerOptions: google.maps.MarkerOptions = { draggable: false };
   timeInterval: any;
 
   constructor(httpClient: HttpClient) {
   }
 
+  
   ngOnInit() {
     this.timeInterval = interval(5000)
       .pipe().subscribe((res: any) => {
         console.log("Maps ships ", this.ships);
       },
         err => console.log(err));
+  }
+
+  click(event: any, ship: Ship)
+  {
+    this.center = { lat: ship.geometry.coordinates[1], lng: ship.geometry.coordinates[0] };    
+    console.log("THIS ", event);
+    console.log("SHIP ", ship);
   }
 }

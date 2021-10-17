30de5f165e2ac1e7217a4121b9f8eb451d786307
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-17 13:50:30 +0300

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { GoogleMapsModule } from '@angular/google-maps';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShipDetailComponent } from './ship-detail/ship-detail.component';
 import { ShipsComponent } from './ships/ships.component';
 import { MapsComponent } from './maps/maps.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     ShipDetailComponent,
     ShipsComponent,
-    MapsComponent,
+    MapsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     FlexLayoutModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]

8d2881a783759d950624bd6a06b29b5bdf94e1f4
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-18 20:35:13 +0300

    Added info-page component

---
+++ src/app/info-page/info-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './info-page.component.html',
+  styleUrls: ['./info-page.component.scss']
+})
+export class InfoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

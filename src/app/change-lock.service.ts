39587618a716579e5c595d732cf4e69aef691585
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-22 16:56:14 +0300

    Added lock class

--- src/app/change-lock.service.ts
+++ src/app/change-lock.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-
-interface Lock {
-  name: string;
-  coordinates: number[];
-}
+import { Lock } from './models/lock';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +7,12 @@ interface Lock {
 
 export class ChangeLockService {
 
-  constructor() { }
-
   public locks: Lock[] = [
     {name: 'Mustola', coordinates: [28.316986, 61.062275]},
     {name: 'Mälkiä', coordinates: [28.304218, 61.070615]},
     {name: 'Soskua', coordinates: [28.400717, 61.039631]}
   ];
   public selectedLock: Lock = this.locks[0];
+  
+  constructor()  {}
 }
---
+++ src/app/models/lock.ts
@@ -0,0 +1,4 @@
+export class Lock {
+    name!: string;
+    coordinates!: number[];
+}

f391341228c6eeaa33b8ad2aa0c8f4af66a80cef
Henrikki Helander <henrikki_helander@hotmail.com>
2021-10-22 17:38:12 +0300

    Changed lock order to correct

--- src/app/change-lock.service.ts
+++ src/app/change-lock.service.ts
@@ -8,11 +8,11 @@ import { Lock } from './models/lock';
 export class ChangeLockService {
 
   public locks: Lock[] = [
-    {name: 'Mustola', coordinates: [28.316986, 61.062275]},
     {name: 'Mälkiä', coordinates: [28.304218, 61.070615]},
+    {name: 'Mustola', coordinates: [28.316986, 61.062275]},
     {name: 'Soskua', coordinates: [28.400717, 61.039631]}
   ];
-  public selectedLock: Lock = this.locks[0];
+  public selectedLock: Lock = this.locks[1];
   
   constructor()  {}
 }
